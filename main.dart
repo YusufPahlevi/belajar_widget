67d33d5dea2daece61978408153258fde157739d
YusufPahlevi <namakuyusuf96@gmail.com>
2024-11-11 10:55:40 +0700

    container

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,68 +8,79 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Root widget
+    return MaterialApp( // Root widget
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Halaman Login'),
+
+          title: Container(child: Text('Container Widget',style: TextStyle(
+            color:Colors.white
+          ),),
+            color: const Color.fromARGB(255, 2, 69, 214),
+            alignment: Alignment.center,
+            width:2200,
+            height: 100,
+            
+            
+          )
         ),
         body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Builder(
-              builder: (context) {
-                return Column(
-                  children: [ 
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FlutterLogo(
-                          size:50
-                        ),
-                        Text(
-                          'Flutter',
-                          style: TextStyle(fontSize: 35, color: Colors.blue),
-                        ),
-                      ],
-                    ),
-                    
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Email',
-                        
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Password',
-                      ),
-                    
-                    ),
-                    const SizedBox(height: 20,),
-                    const Text('forgot password?',style: TextStyle(color: Colors.red),),
-                    const SizedBox(height: 20,),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue
-                      ),
-                      onPressed: () {
-                        print('login berasil');
-                      },                  
-                      child: const Text('Login'), 
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(50),
-                      child: const Text('new user? get account'),
-                    )
-                  ],
-                );
-              },
-            ),
+          child: Builder(
+            builder: (context) {
+              return Column(
+                children: [
+               Center(
+        child: Container(
+          height: 440,
+          width: 850,
+          margin: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 16, 58, 197),
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: const Color.fromARGB(255, 17, 106, 223)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 8,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Title: Flutter Tutoral',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              const SizedBox(height: 8),
+              const Divider(color: Colors.black),
+              const Text(
+                'Description: This is a card-like layout using a Container widget. It has padding, margin, and a box shadow.',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('visit to read more')))
+            ],
+          ),
+        ),
+      ),
+  
+       
+                ],
+              );
+            },
           ),
         ),
       ),
