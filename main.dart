6038648510e11ae65bf5ff0b76abe12e1f51d0e4
YusufPahlevi <namakuyusuf96@gmail.com>
2024-11-18 07:29:29 +0700

    flutter layout pemandangan alam,hasil akhir

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,82 +7,182 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( // Root widget
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      title: appTitle,
       home: Scaffold(
         appBar: AppBar(
-
-          title: Container(child: Text('Container Widget',style: TextStyle(
-            color:Colors.white
-          ),),
-            color: const Color.fromARGB(255, 2, 69, 214),
-            alignment: Alignment.center,
-            width:2200,
-            height: 100,
-            
-            
-          )
+          title:Container(child: Text('Pemandangan Alam'),),
+          
         ),
-        body: Center(
-          child: Builder(
-            builder: (context) {
-              return Column(
-                children: [
-               Center(
-        child: Container(
-          height: 200,
-          width: 850,
-          margin: const EdgeInsets.all(16.0),
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 16, 58, 197),
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(color: const Color.fromARGB(255, 17, 106, 223)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                spreadRadius: 2,
-                blurRadius: 8,
-                offset: const Offset(0, 4),
-              ),
-            ],
-          ),
+        body: const SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                'Title: Flutter Tutoral',
-                style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
+              ImageSection(
+                image: 'assets/images/herman.jpg'
               ),
-              const SizedBox(height: 8),
-              const Divider(color: Colors.black),
-              const Text(
-                'Description: This is a card-like layout using a Container widget. It has padding, margin, and a box shadow.',
-                style: TextStyle(
-                  color: Colors.white,
+              TitleSection(
+                name: 'Pemandangan', 
+                location: 'Lawang,'
+              ),
+              ButtonSection(),
+              TextSection(
+                description:
+                    'pemandanggan depan rumah pada sore hari setelah hujan',
+             ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                  height: 40,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('visit to read more')))
-            ],
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+  }
+}
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: color,
+            icon: Icons.call,
+            label: 'CALL',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: 'ROUTE',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
           ),
         ),
+      ],
+    );
+  }
+}
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
       ),
-  
-       
-                ],
-              );
-            },
-          ),
-        ),
-      ),
+    );
+  }
+}
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
     );
   }
 }
